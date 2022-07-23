ab6400de8251f1966a394df740394bfbbd00ea50
MustafaBasar471 <mustafabasar471@gmail.com>
2022-07-23 15:54:48 +0300

    Frist Commit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { Routes, Route, useNavigate } from 'react-router-dom';
+import { useEffect } from 'react';
+import { gapi } from "gapi-script"
+
+import Login from "./components/Login";
+import Home from "./container/Home";
+
+const App = () => {
+  
+  return (
+    <Routes>
+      <Route path='login' element={<Login />} />
+      <Route path='/*' element={<Home />} />
+    </Routes>
+  )
+}
+
+export default App
